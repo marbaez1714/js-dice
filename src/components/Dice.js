4b78780b8dded5ba0e218c692ad8bb661513a1a9
marbaez1714 <marbaez1714@gmail.com>
2019-06-13 23:56:00 -0500

    ANIMATIONS

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -3,38 +3,56 @@ import React from "react";
 export default function Dice(props) {
   const { number } = props;
 
-  const onePip = number === 1 && "diePip";
-  const threePip = number === 3 && "diePip";
-  const fourPip = number === 4 && "diePip";
-  const fivePip = number === 5 && "diePip";
-  const sixPip = number === 6 && "diePip";
+  const findPip = pips => {
+    const isFilled = pips.reduce((included, pip) => (pip === number ? true : included), false);
+    const pipName = isFilled ? "diePip" : "";
+    return pipName;
+  };
 
-  const cornerPip = number > 1 && "diePip";
+  const findAnimation = num => {
+    switch (num) {
+      case 1:
+        return "one";
+      case 2:
+        return "two";
+      case 3:
+        return "three";
+      case 4:
+        return "four";
+      case 5:
+        return "five";
+      case 6:
+        return "six";
+
+      default:
+        break;
+    }
+  };
 
   return (
-    <div className="dieBox">
+    <div className={`dieBox ${findAnimation(number)}`}>
       <div className="dieCol left">
-        <div className={`dieGrid ${cornerPip}`} />
+        <div className={`dieGrid ${findPip([2, 3, 4, 5, 6])}`} />
         <div className="dieGrid" />
-        <div className={`dieGrid ${sixPip}`} />
+        <div className={`dieGrid ${findPip([6])}`} />
         <div className="dieGrid" />
-        <div className={`dieGrid ${sixPip || fourPip || fivePip}`} />
+        <div className={`dieGrid ${findPip([4, 5, 6])}`} />
       </div>
       <div className="dieCol filler" />
       <div className="dieCol ">
-        <div className={`dieGrid ${sixPip}`} />
+        <div className={`dieGrid ${findPip([6])}`} />
         <div className="dieGrid" />
-        <div className={`dieGrid ${onePip || sixPip || threePip || fivePip} `} />
+        <div className={`dieGrid ${findPip([1, 3, 5, 6])} `} />
         <div className="dieGrid" />
-        <div className={`dieGrid ${sixPip}`} />
+        <div className={`dieGrid ${findPip([6])}`} />
       </div>
       <div className="dieCol filler" />
       <div className="dieCol ">
-        <div className={`dieGrid ${sixPip || fourPip || fivePip}`} />
+        <div className={`dieGrid ${findPip([4, 5, 6])}`} />
         <div className="dieGrid" />
-        <div className={`dieGrid ${sixPip} `} />
+        <div className={`dieGrid ${findPip([6])}`} />
         <div className="dieGrid" />
-        <div className={`dieGrid ${cornerPip}`} />
+        <div className={`dieGrid ${findPip([2, 3, 4, 5, 6])}`} />
       </div>
     </div>
   );
