7ea888f7474c731b8ba8e6a78d2f81d023e3fd41
marbaez1714 <marbaez1714@gmail.com>
2019-06-13 23:12:06 -0500

    Updates

---
+++ precache-manifest.7e67e838b2fea7d833473a8ac31d539d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5869abb192d8f2ed96953c432971d60",
+    "url": "/js-dice/index.html"
+  },
+  {
+    "revision": "50d81c97fc9a1c5c8e5a",
+    "url": "/js-dice/static/css/main.f8765021.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/js-dice/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "50d81c97fc9a1c5c8e5a",
+    "url": "/js-dice/static/js/main.7fe66157.chunk.js"
+  },
+  {
+    "revision": "9c0e9a9110ee6d0a6019",
+    "url": "/js-dice/static/js/runtime~main.4c33e043.js"
+  }
+]);
